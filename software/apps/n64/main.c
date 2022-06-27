97b8da0a29cccac1af104e4495cf500c58648a05
Konrad Beckmann <konrad.beckmann@gmail.com>
2022-06-27 23:25:49 +0200

    More n64 chaos, add notes

--- software/apps/n64/main.c
+++ software/apps/n64/main.c
@@ -56,15 +56,15 @@ void core1_scanline_callback() {
 	scanline = (scanline + 1) % FRAME_HEIGHT;
 }
 
-uint32_t ringbuf[1024*1];
+uint32_t ringbuf[1024*8];
 uint32_t ringbuf_ctr;
 uint32_t ringbuf_idx;
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
 
-#define CAPTURE_SIZE (1024*8)
-uint32_t capture_buf1[CAPTURE_SIZE];
-uint32_t capture_buf2[CAPTURE_SIZE];
+#define CAPTURE_SAMPLES (1024*4)
+uint32_t capture_buf1[CAPTURE_SAMPLES];
+uint32_t capture_buf2[CAPTURE_SAMPLES];
 uint32_t *capture_buf = capture_buf1;
 uint32_t *read_buf = capture_buf2;
 
@@ -104,8 +104,7 @@ void dma_handler(void)
 	dma_hw->ints0 = 1u << dma_chan;
 
 	if (!dma_buf_consumed) {
-		// Application is lagging behind!
-		printf(!"Shieeet!\r\n");
+		printf("Application is lagging behind!\r\n");
 		// don't start another DMA
 		return;
 	}
@@ -164,12 +163,6 @@ int main() {
 
 	printf("Start rendering\n");
 
-	// for (int y = 0; y < FRAME_HEIGHT; y++) {
-	// 	for (int x = 0; x < FRAME_WIDTH; x++) {
-	// 		framebuf[y*FRAME_WIDTH + x] = x + y;
-	// 	}
-	// }
-
 	for (int i = 0; i <= 8; i++) {
 		gpio_init(i);
 		gpio_set_dir(i, GPIO_IN);
@@ -188,8 +181,7 @@ int main() {
 	// Grant high bus priority to the DMA, so it can shove the processors out
 	// of the way. This should only be needed if you are pushing things up to
 	// >16bits/clk here, i.e. if you need to saturate the bus completely.
-	// bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
-
+	bus_ctrl_hw->priority = BUSCTRL_BUS_PRIORITY_DMA_W_BITS | BUSCTRL_BUS_PRIORITY_DMA_R_BITS;
 
 	dma_chan = dma_claim_unused_channel(true);
 	printf("dma_chan=%d\r\n", dma_chan);
@@ -201,8 +193,8 @@ int main() {
 	channel_config_set_ring(&c, false, 4);
 	channel_config_set_read_increment(&c, false);
 
-	// write 0, 1, ... 
-	// channel_config_set_ring(&c, false, ARRAY_SIZE(capture_buf1));
+	// write 0, 1, 2, ... <stop>
+	// channel_config_set_ring(&c, true, ARRAY_SIZE(capture_buf1));
 	channel_config_set_write_increment(&c, true);
 	channel_config_set_dreq(&c, pio_get_dreq(pio, sm, false));
 
@@ -225,187 +217,186 @@ int main() {
 	int count = 0;
 	int row = 0;
 	int column = 0;
-	int should_print;
 
 	uint32_t BGRS_r = 0;
 
-	#define RGB888_TO_RGB565(r, g, b) (\
-		(((r >> 3) &0x1f) << 11) | \
-		(((g)&0x3f) << 5) | \
-		((b)&0x1f)\
-	)
-
-	// #define NOT_CSYNC_POS 31
-	// #define NOT_HSYNC_POS 30
-	// #define NOT_CLAMP_POS 29
-	// #define NOT_VSYNC_POS 28
 
 	#define NOT_CSYNC_POS (0)
 	#define NOT_HSYNC_POS (1)
 	#define NOT_CLAMP_POS (2)
 	#define NOT_VSYNC_POS (3)
 
+	uint8_t sync = 0;
+	uint8_t sync_r = 0;
 
-	// state = STATE_INVALID;
+	int is_synced = 0;
+	int sync_loops = 0;
+	uint32_t sync_patterns[16];
 
-	uint8_t sync;
-	uint8_t sync_r;
+	memset(sync_patterns, 0x00, sizeof(sync_patterns));
+
+typedef enum {
+							// 3519: 0 0 1 1: 695 
+							// 3520: 1 1 1 1: 58 
+							// 3521: 1 1 0 1: 6 
+							// 3522: 1 1 1 1: 35 
+							// 3523: 0 0 1 1: 695 
+							// 3524: 1 1 1 1: 58 
+							// 3525: 1 1 0 1: 6 
+							// 3526: 1 1 1 1: 35 
+
+							// 3527: 0 0 1 1: 695 
+							// 3528: 1 1 1 1: 58 
+							// 3529: 0 0 1 1: 736 
+							// 3530: 1 1 1 1: 58 
+							// 3531: 0 0 1 1: 736 
+							// 3532: 1 1 1 1: 58 
+
+
+	STATE_ROW_FP,           // 1111 58  ROW_ACTIVE      -> ROW_FP
+	STATE_ROW_SYNC,         // 1101 6   ROW_FP          -> ROW_SYNC
+	STATE_ROW_BP,           // 1111 35  ROW_SYNC        -> ROW_BP
+	STATE_ROW_ACTIVE,       // 0011 695 ROW_BP          -> ROW_ACTIVE -> ROW_FP (loop)
+
+	STATE_BLANK1,           // 0011 736 ROW_FP          -> BLANK1
+	STATE_BLANK2,           // 1111 58  BLANK1          -> BLANK2
+	STATE_BLANK3,           // 0011 736 BLANK2          -> BLANK3
+	STATE_BLANK4,           // 1111 58  BLANK3          -> BLANK4
+
+							// Let's ignore all pixels with ~VS=0
+							// 3533: 0 0 1 0: 736 
+							// 3534: 0 1 1 0: 58 
+							// 3535: 1 1 1 0: 678 
+							// 3536: 0 1 1 0: 58 
+							// 3537: 0 0 1 0: 2 
+							// 3538: 0 1 1 0: 57 
+							// 3539: 1 1 1 0: 678 
+							// 3540: 0 0 1 0: 58 
+							// 3541: 0 1 1 0: 58 
+	STATE_BLANK5,           // xxx0 2383  BLANK4        -> BLANK5
+
+							// 3542: 0 1 1 1: 339 <--- Really weird ones, notice 0b0111
+							// 3543: 0 0 1 1: 397 <--- Really weird ones
+							// 3544: 1 1 1 1: 58 
+							// 3545: 0 0 1 1: 736 <--- Two extra wide ones
+							// 3546: 1 1 1 1: 58 
+							// 3547: 0 0 1 1: 736 <--- Two extra wide ones
+	STATE_BLANK6,           // 0111 339	BLANK5           -> BLANK6
+	STATE_BLANK7,           // 0011 397	BLANK6           -> BLANK7
+	STATE_BLANK8,           // 1111 58	BLANK7           -> BLANK8
+	STATE_BLANK9,           // 0011 736	BLANK8           -> BLANK9
+	STATE_BLANK10,          // 1111 58	BLANK9           -> BLANK10
+	STATE_BLANK11,          // 0011 736	BLANK10          -> BLANK11 -> STATE_ROW_FP [Start Of Frame]
+
+							// -> STATE_ROW_FP [Start Of Frame]
+							// 3548: 1 1 1 1: 58 
+							// 3549: 1 1 0 1: 6 
+							// 3550: 1 1 1 1: 35 
+							// 3551: 0 0 1 1: 695 <---- now it's normal again
+							// 3552: 1 1 1 1: 58 
+							// 3553: 1 1 0 1: 6 
+							// 3554: 1 1 1 1: 35 
+							// 3555: 0 0 1 1: 695 
+							// 3556: 1 1 1 1: 58 
+};
+
+
+
+
 
 	while (1) {
-#if 0
-		// Grab n64 video data
-		uint32_t BGRS = pio_sm_get_blocking(pio, 0);
-
-		sync = BGRS & 0xFF;
-		count++;
-
-		if (sync != sync_r) {
-			if (sync == 0b1100)
-				ringbuf_put(sync | (count << 8));
-			count = 0;
-		}
-
-		sync_r = sync;
-
-#elif 1
-
-		// Handle everything in DMA
+		// Wait for DMA transfer to finish
 		__wfi();
 
-		// for (int i = 0; i < ARRAY_SIZE(capture_buf1); i++) {
-		for (int i = 0; i < 1024*2; i++) {
-			// Grab n64 video data and increment the pointer
-			uint32_t BGRS = *(read_buf++);
+		if (dma_buf_consumed) {
+			// The handled irq wasn't our DMA transfer (dma_handler sets this to 0)
+			return;
+		}
 
+		// Make a copy of the read_buf pointer - DMA irq should finish before we're done.
+		uint32_t *consume_buf = read_buf;
+
+		for (int i = 0; i < CAPTURE_SAMPLES; i++) {
+			// Grab n64 video data and increment the pointer
+			uint32_t BGRS = *(consume_buf++);
+
+#if 1
 			sync = BGRS & 0xFF;
 			count++;
 
-			if (sync != sync_r) {
-				if (sync == 0b1100)
-					ringbuf_put(sync | (count << 8));
+			#define MASK      (0b1010)
+			#define PATTERN_R (0b1000) // VSYNC | ~HSYNC
+			#define PATTERN   (0b1010) // VSYNC |  HSYNC
+
+			// if (((sync_r & MASK) == PATTERN_R) && ((sync & MASK) == PATTERN)) {
+			if (sync_r != sync) {
+				ringbuf_put(sync | (count << 8));
 				count = 0;
 			}
 
 			sync_r = sync;
 
+#elif 1
 
-#if 0
-			if (  BGRS_r == 0b1111 && BGRS == 0b1100)
-				//  (BGRS   & (1 << NOT_VSYNC_POS)) &&
-				//  !(BGRS_r & (1 << NOT_HSYNC_POS)) && (BGRS & (1 << NOT_HSYNC_POS))) {
-				{
-				// VSync && negedge HSync
-				column = 0;
-			} else if (BGRS == 0b0011) {
-				// HSync stays high
-				column++;
+			sync = BGRS & 0xFF;
+			count++;
+
+
+			if (!is_synced) {
+				int is_hsync = !(BGRS & NOT_HSYNC_POS);
+				int is_vsync = !(BGRS & NOT_HSYNC_POS);
+
+				int was_hsync = !(BGRS_r & NOT_HSYNC_POS);
+				int was_vsync = !(BGRS_r & NOT_HSYNC_POS);
+
+				sync_patterns[sync]++;
+
+
+				// if (sync_r != sync) {
+				// 	sync_patterns[sync_r & 0xF] = count;
+				// 	count = 0;
+
+				// 	sync_loops++;
+
+				// 	if (sync_loops == 1024000) {
+				// 		sync_loops = 0;
+				// 		for (int i = 0; i < 16; i++) {
+				// 			printf("Sync[%02X] = %d\r\n", i, sync_patterns[i]);
+				// 		}
+				// 	}
+				// }
+
+				sync_r = sync;
 			}
 
-			if (!(BGRS_r & (1 << NOT_VSYNC_POS)) && (BGRS & (1 << NOT_VSYNC_POS))) {
-				// posedge VSync
-				row = 0;
-				should_print = 1;
-			} else if (BGRS & (1 << NOT_VSYNC_POS)) {
-				// VSync stays high
-				row++;
-			}
 
-			// Limit bounds
-			if (row >= FRAME_HEIGHT) {
-				row = FRAME_HEIGHT - 1;
-			}
-
-			if (column >= FRAME_WIDTH) {
-				column = FRAME_WIDTH - 1;
-			}
-
-			BGRS_r = BGRS;
-
-			// if (BGRS & 0xFF == 0b1110) {
-				// framebuf[row * FRAME_WIDTH + column] = RGB888_TO_RGB565(
-				// 	BGRS >> 24,
-				// 	BGRS >> 8,
-				// 	BGRS);
-			// }
+#else
 
 			// framebuf[row * FRAME_WIDTH + count++] = RGB888_TO_RGB565(
 			// if ((BGRS & 0b1110) == 0b1110) {
+				framebuf[count] = ( (BGRS >> 26) & 0x1f);
+				// framebuf[row * FRAME_WIDTH + column] =
+					// (((BGRS >> 10) & 0x1f) << 11) |
+					// (((BGRS >> 16) & 0x3f) << 5) |
+					// ( (BGRS >> 26) & 0x1f);
 
-				// framebuf[count++] = 
-				framebuf[row * FRAME_WIDTH + column] =
-					(((BGRS >> 10) & 0x1f) << 11) |
-					(((BGRS >> 16) & 0x3f) << 5) |
-					( (BGRS >> 26) & 0x1f);
+				count++;
 
-				if (count >= FRAME_WIDTH * FRAME_HEIGHT) {
-					count = 0;
-				}
+				// if (count >= FRAME_WIDTH * FRAME_HEIGHT) {
+				// 	count = 0;
+				// }
 			// }
 
 #endif
+		}
+
+		if (count >= FRAME_WIDTH * FRAME_HEIGHT - CAPTURE_SAMPLES) {
+			count = 0;
 		}
 
 		// Inform the DMA handler that we are done
 		dma_buf_consumed = 1;
 
-
-#else
-
-		// Grab n64 video data
-		uint32_t BGRS = pio_sm_get_blocking(pio, 0);
-
-		if (  BGRS_r == 0b1111 && BGRS == 0b1100)
-			//  (BGRS   & (1 << NOT_VSYNC_POS)) &&
-			//  !(BGRS_r & (1 << NOT_HSYNC_POS)) && (BGRS & (1 << NOT_HSYNC_POS))) {
-			{
-			// VSync && negedge HSync
-			column = 0;
-		} else if (BGRS == 0b0011) {
-			// HSync stays high
-			column++;
-		}
-
-		if (!(BGRS_r & (1 << NOT_VSYNC_POS)) && (BGRS & (1 << NOT_VSYNC_POS))) {
-			// posedge VSync
-			row = 0;
-			should_print = 1;
-		} else if (BGRS & (1 << NOT_VSYNC_POS)) {
-			// VSync stays high
-			row++;
-		}
-
-		// Limit bounds
-		if (row >= FRAME_HEIGHT) {
-			row = FRAME_HEIGHT - 1;
-		}
-
-		if (column >= FRAME_WIDTH) {
-			column = FRAME_WIDTH - 1;
-		}
-
-		BGRS_r = BGRS;
-
-		// if (BGRS & 0xFF == 0b1110) {
-			// framebuf[row * FRAME_WIDTH + column] = RGB888_TO_RGB565(
-			// 	BGRS >> 24,
-			// 	BGRS >> 8,
-			// 	BGRS);
-		// }
-
-		// framebuf[row * FRAME_WIDTH + count++] = RGB888_TO_RGB565(
-		if ((BGRS & 0b1110) == 0b1110) {
-
-			// framebuf[count++] = 
-			framebuf[row * FRAME_WIDTH + column] =
-				(((BGRS >> 10) & 0x1f) << 11) |
-				(((BGRS >> 16) & 0x3f) << 5) |
-				( (BGRS >> 26) & 0x1f);
-
-			if (count >= FRAME_WIDTH * FRAME_HEIGHT) {
-				count = 0;
-			}
-		}
-#endif
 	}
 	__builtin_unreachable();
 }
